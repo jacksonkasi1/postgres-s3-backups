5a2663293cd24d2980be4738236ad667ff48f250
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-07-04 15:27:57 +0530

    update to api formate

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-import { envsafe, str } from "envsafe";
+import { envsafe, str, num } from "envsafe";
 
 export const env = envsafe({
   AWS_ACCESS_KEY_ID: str(),
@@ -8,14 +8,18 @@ export const env = envsafe({
   BACKUP_DATABASE_URL: str({
     desc: 'The connection string of the database to backup.'
   }),
+  AWS_S3_ENDPOINT: str({
+    desc: 'The S3 custom endpoint you want to use.',
+    default: '',
+    allowEmpty: true,
+  }),
   BACKUP_CRON_SCHEDULE: str({
     desc: 'The cron schedule to run the backup on.',
     default: '0 5 * * *',
     allowEmpty: true
   }),
-  AWS_S3_ENDPOINT: str({
-    desc: 'The S3 custom endpoint you want to use.',
-    default: '',
-    allowEmpty: true,
+  PORT: num({
+    desc: 'The port to run the backup API on.',
+    default: 5000,
   }),
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
-import { CronJob } from "cron";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+import express, { Application, Request, Response } from "express";
 import { backup } from "./backup";
 import { env } from "./env";
 
-const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+// Create an Express application
+const app: Application = express();
+const port: number = env.PORT || 5000;
+
+// Define the endpoint to trigger the backup
+app.get("/backup", async (req: Request, res: Response) => {
   try {
     await backup();
+    res.status(200).send("Backup completed successfully");
   } catch (error) {
-    console.error("Error while running backup: ", error)
+    console.error("Error while running backup: ", error);
+    res.status(500).send("Error while running backup");
   }
 });
 
-job.start();
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Backup API is running at http://localhost:${port}`);
+});
 
-console.log("Backup cron scheduled...")
+// import { CronJob } from "cron";
+
+// const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+//   try {
+//     await backup();
+//   } catch (error) {
+//     console.error("Error while running backup: ", error)
+//   }
+// });
+
+// job.start();
